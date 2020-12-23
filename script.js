7a83df307e40173558ccf2b19497db7bd23b5512
charlotteeed <c.diondufourd@gmail.com>
2020-12-23 11:36:06 -0500

    broken javascript to debug

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
-window.onload = function () {
+//var elmnt1, elmnt2, elmnt3;
+
+$(function () {
   // reset the scroll position
-  window.scroll(0, 0); 
-  
-  
-  var elmnt1 = document.querySelector("#elem-1");
-  var elmnt2 = document.querySelector("#elem-2");
-  var elmnt3 = document.querySelector("#elem-3");
+  window.scroll(0, 0);
+  var elmnt1 = $("#elem-1");
+  var elmnt2 = $("#elem-2");
+  var elmnt3 = $("#elem-3");
+
+//  elmnt1.get(0).scrollIntoView({
+//    behavior: 'smooth'
+//  });
 
 
-//  function scrollToElem(e) {
-//    e.nextElementSibling.scrollIntoView({
-//      behavior: 'smooth'
-//    });
-//  }
-//
-//  elmnt1.onclick = scrollToElem(elmnt1);
-//  elmnt2.onclick = scrollToElem(elmnt2);
-//  elmnt3.onclick = scrollToElem(elmnt3);
-//
 
 
-console.log(elmnt1.innerHTML);
-  
-    elmnt1.onclick = function () {
-      elmnt2.scrollIntoView({
-        behavior: 'smooth'
-      });
-  
-    };
-  
-    elmnt2.onclick = function () {
-      elmnt3.scrollIntoView({
-        behavior: 'smooth'
-      });
-  
-    };
-    elmnt3.onclick = function () {
-      elmnt1.scrollIntoView({
-        behavior: 'smooth'
-      });
-  
-    };
-};
+  function scrollToElem(e) {
+    e.get(0).scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+
+  elmnt1.click(scrollToElem(elmnt2));
+  elmnt2.click(scrollToElem(elmnt3));
+  elmnt3.click(scrollToElem(elmnt1));
+
+
+  //    elmnt2.onclick = scrollToElem(elmnt3);
+  //    elmnt3.onclick = scrollToElem(elmnt1);
+
+
+
+  //console.log(elmnt1.innerHTML);
+
+  //  elmnt1.get(0).onclick = function () {
+  //    elmnt2.get(0).scrollIntoView({
+  //      behavior: 'smooth'
+  //    });
+  //
+  //  };
+  //
+  //  elmnt2.get(0).onclick = function () {
+  //    elmnt3.get(0).scrollIntoView({
+  //      behavior: 'smooth'
+  //    });
+  //
+  //  };
+  //  elmnt3.get(0).onclick = function () {
+  //    elmnt1.get(0).scrollIntoView({
+  //      behavior: 'smooth'
+  //    });
+  //
+  //  };
+});
